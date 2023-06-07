7651709d64acae3f043a3c39562ea7bfc8781d3b
ManishThulung <manish.thulung58@gmail.com>
2023-06-07 23:59:47 +0545

    nav and connect

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,18 +1,55 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useMoralis } from "react-moralis";
 
 function Navbar() {
-  const { enableWeb3 } = useMoralis();
+  const {
+    enableWeb3,
+    account,
+    isWeb3Enabled,
+    Moralis,
+    deactivateWeb3,
+    isWeb3EnableLoading,
+  } = useMoralis();
+
+  useEffect(() => {
+    if (isWeb3Enabled) return;
+
+    if (typeof window !== "undefined") {
+      if (window.localStorage.getItem("connected")) {
+        enableWeb3();
+      }
+    }
+  }, [isWeb3Enabled]);
+
+  useEffect(() => {
+    Moralis.onAccountChanged((account) => {
+      console.log(account, "asd");
+
+      if (account == null) {
+        window.localStorage.removeItem("connected");
+        deactivateWeb3();
+        console.log("here");
+      }
+    });
+  }, []);
+
   return (
     <>
-      <button
-        onClick={async () => {
-          await enableWeb3();
-        }}
-        className="bg-blue-800 text-white text-3xl font-normal"
-      >
-        Connect
-      </button>
+      {account ? (
+        <div>Connected to {account}</div>
+      ) : (
+        <button
+          onClick={async () => {
+            await enableWeb3();
+
+            window.localStorage.setItem("connected", "injected");
+          }}
+          className="bg-blue-800 text-white text-3xl font-normal"
+          disabled={isWeb3EnableLoading}
+        >
+          Connect
+        </button>
+      )}
     </>
   );
 }
